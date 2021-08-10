7f917c3f27c5910a7b2ff0d3b984bdaf8febca46
Marcos Saavedra <marcsaav96@gmail.com>
2021-08-10 13:05:10 -0700

    First migration completed

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', (tbl) => {
+    tbl.increments('car_id')
+    tbl.string('vin', 128).unique().notNullable()
+    tbl.string('make', 128).notNullable()
+    tbl.string('model', 128).notNullable()
+    tbl.numeric('milage').notNullable()
+    tbl.string('title', 256)
+    tbl.string('transmission')
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
